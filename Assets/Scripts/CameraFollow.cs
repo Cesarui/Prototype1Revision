5ff63b6f07101e389c0a601826fdcef031fac108
Cesar Pimentel <cpimentelortiz23@gmail.com>
2026-02-07 18:55:17 -0500

    initial commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Camera follows the driver with an offset.
+
+    public Transform driver;
+
+    public Vector3 cameraOffset = new Vector3(0, 5, -10);
+
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.position = driver.position + cameraOffset;
+    }
+}
---
+++ Assets/Scripts/DriverController.cs
@@ -0,0 +1,31 @@
+ using UnityEngine;
+
+public class DriverController : MonoBehaviour
+{
+    // Notes: Vector3 is a way to represent 3D space coordinates.
+
+    public int speed = 40;
+    public float turnSpeed = 0;
+    public float horizontalInput;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Will move the vehicle forward.
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        // horizontalInput can only be -1, 0 or 1 which is why the car won't move even if turnSpeed is above 0 because if horizontal is 0
+        // it will all be 0.
+        transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
+        //Debug.Log(transform.position.ToString());
+
+        if (horizontalInput != 0)
+        {
+            Debug.Log("The player is turning at: " + transform.rotation.ToString());
+        }
+    }
+}
