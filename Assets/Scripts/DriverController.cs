c5912da86527c1ffe3fd573b85da1357f116b25d
Cesar Pimentel <cpimentelortiz23@gmail.com>
2026-02-07 20:50:19 -0500

    add comments and debug.log lines for feedback during Play Mode

--- Assets/Scripts/DriverController.cs
+++ Assets/Scripts/DriverController.cs
@@ -7,25 +7,34 @@ public class DriverController : MonoBehaviour
     public int speed = 40;
     public float turnSpeed = 0;
     public float horizontalInput;
+    public float forwardInput;
     void Start()
     {
-        
+        Debug.Log("Starting position: " + transform.position.ToString());
     }
 
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
 
         // Will move the vehicle forward.
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * forwardInput);
         // horizontalInput can only be -1, 0 or 1 which is why the car won't move even if turnSpeed is above 0 because if horizontal is 0
         // it will all be 0.
-        transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
         //Debug.Log(transform.position.ToString());
 
+
+        // Added these if statements just for information.
         if (horizontalInput != 0)
         {
-            Debug.Log("The player is turning at: " + transform.rotation.ToString());
+            Debug.Log("Current Rotation along the Y: " + transform.rotation.y.ToString() 
+                 + "\nCurrent Position along the Z: " + transform.position.z.ToString());
+        }
+        if (transform.position.y > 10)
+        {
+            Debug.Log("Player is OFF the ground");
         }
     }
 }
